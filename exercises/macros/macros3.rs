b2b93e7cce8ab9131c62fb109d5078ca53e67a7b
jiake <2686181617@qq.com>
2024-10-03 14:16:08 +0800

    test

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,16 +5,15 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+#![feature(decl_macro)]
 mod macros {
-    macro_rules! my_macro {
+    pub macro my_macro {
         () => {
             println!("Check out my macro!");
-        };
+        }
     }
 }
 
 fn main() {
-    my_macro!();
+    macros::my_macro!();
 }
